320bad849064f1f9c048072817b977fd9dda5047
alpha2phi <alpha2phi@gmail.com>
2023-05-06 22:01:04 +0800

    feat(wip): initial commit

--- init.lua
+++ init.lua
@@ -1,58 +1,16 @@
-                                                                                         _                         
-require "config.options"
-require "config.lazy"
+require("config.options")
+require("config.lazy")
 
-vim.api.nvim_create_autocmd("User", {
-  pattern = "VeryLazy",
-  callback = function()
-    require "config.autocmds"
-    require "config.keymaps"
-    require "utils.contextmenu"
-  end,
-})
-                                                                                     ,--.\`-. __                   
-                                                                                   _,.`. \:/,"  `-._               
-                                                                               ,-*" _,.-;-*`-.+"*._ )              
-                                                                              ( ,."* ,-" / `.  \.  `.              
-                                                                             ,"   ,;"  ,"\../\  \:   \             
-                                                                            (   ,"/   / \.,' :   ))  /             
-                                                                             \  |/   / \.,'  /  // ,'              
-                                                                              \_)\ ,' \.,'  (  / )/                
-                                                                                  `  \._,'   `"                    
-                                                                                     \../                          
-                                                                                     \../                          
-                                                                           ~        ~\../           ~~             
-                                                                    ~~          ~~   \../   ~~   ~      ~~         
-                                                               ~~    ~   ~~  __...---\../-...__ ~~~     ~~         
-                                                                 ~~~~  ~_,--'        \../      `--.__ ~~    ~~     
-                                                             ~~~  __,--'              `"             `--.__   ~~~  
-                                                          ~~  ,--'                                         `--.    
-                                                             '------......______             ______......------` ~~
-                                                           ~~~   ~    ~~      ~ `````---"""""  ~~   ~     ~~       
-                                                                  ~~~~    ~~  ~~~~       ~~~~~~  ~ ~~   ~~ ~~~  ~  
-                                                               ~~   ~   ~~~     ~~~ ~         ~~       ~~   SSt    
-                                                                        ~        ~~       ~~~       ~              
-                                                            
-
-
-                                                                Find file                                     f
-
-                                                                New file                                      n
-
-                                                                Recent files                                  r
-
-                                                                Find text                                     g
-
-                                                                Config                                        c
-
-                                                              勒 Restore Session                               s
-
-                                                              鈴 Lazy                                          l
-
-                                                                Quit                                          q
-
-                                                             	   v0.9.0	⚡Neovim loaded 146 plugins in 58.95ms
-                                                              
-                                                              Software is hard.
-                                                              
-                                                                                                  - Donald Knuth
+if vim.fn.argc(-1) == 0 then
+	vim.api.nvim_create_autocmd("User", {
+		group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
+		pattern = "VeryLazy",
+		callback = function()
+			require("config.autocmds")
+			require("config.keymaps")
+		end,
+	})
+else
+	require("config.autocmds")
+	require("config.keymaps")
+end
