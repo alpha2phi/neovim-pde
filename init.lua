6748e8a8860d5b653aee0e4ac3f4ac6420d7e411
alpha2phi <alpha2phi@gmail.com>
2023-05-07 09:55:30 +0800

    feat(wip): initial commit

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
-require("config.options")
-require("config.lazy")
+require "config.options"
+require "config.lazy"
 
 if vim.fn.argc(-1) == 0 then
-	vim.api.nvim_create_autocmd("User", {
-		group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
-		pattern = "VeryLazy",
-		callback = function()
-			require("config.autocmds")
-			require("config.keymaps")
-		end,
-	})
+  vim.api.nvim_create_autocmd("User", {
+    group = vim.api.nvim_create_augroup("NeovimPDE", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+      require "config.autocmds"
+      require "config.keymaps"
+    end,
+  })
 else
-	require("config.autocmds")
-	require("config.keymaps")
+  require "config.autocmds"
+  require "config.keymaps"
 end
---
+++ lua/base/init.lua
@@ -0,0 +1,8 @@
+return {
+  -- Git related plugins
+  { "tpope/vim-fugitive", }
+  { "tpope/vim-rhubarb", }
+
+  -- Detect tabstop and shiftwidth automatically
+  { "tpope/vim-sleuth" },
+}
