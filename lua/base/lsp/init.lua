0d4f3747dfe2ce7bfeabafb21f45d48ced9410a9
alpha2phi <alpha2phi@gmail.com>
2023-07-09 16:55:50 +0800

    fix: rust lsp

--- lua/base/lsp/init.lua
+++ lua/base/lsp/init.lua
@@ -4,7 +4,7 @@ return {
     event = "VeryLazy",
     opts = {
       defaults = {
-        ["<leader>l"] = { name = "+LSP" },
+        ["<leader>l"] = { name = "+Language" },
       },
     },
   },
